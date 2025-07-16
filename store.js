329d102cb09b4d9dca88a541fae9bc1f27ad0461
Somya Jain <somyajain20048@gmail.com>
2025-07-16 22:44:55 +0530

    update store.js

--- store.js
+++ store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
 import usernameReducer from "./usernameSlice";
 import themeReducer from "./ThemeSlice";
+import cartReducer from "./CartSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     username: usernameReducer,
     theme: themeReducer,
+    cart: cartReducer,
   },
 });
